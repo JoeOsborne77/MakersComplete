83af2c19299cb4dfe8704239660c109d60b63d28
JoeOsborne77 <joeosborne77@gmail.com>
2022-10-30 15:03:58 +0000

    diagram added

--- Week_7/NewsChallenge/src/GuardianApi.js
+++ Week_7/NewsChallenge/src/GuardianApi.js
@@ -1,7 +1,6 @@
 class GuardianApi {
   constructor() {
-    this.apiURL =
-      "https://content.guardianapis.com/search?api-key=55e5d388-8b9e-477c-b420-c20a69c2be80&show-fields=thumbnail";
+    this.apiURL = `https://content.guardianapis.com/search?api-key=${apikey}&show-fields=thumbnail`;
   }
   loadArticles(callback) {
     fetch(this.apiURL)
@@ -10,30 +9,6 @@ class GuardianApi {
         callback(data);
       });
   }
-
-  // searchArticles() {
-  //   this.apiArr = [];
-  //   const articleCardTemplate = document.querySelector(
-  //     "[data-article-template]"
-  //   );
-
-  //   const articleCardContainer = document.querySelector(
-  //     "[data-articles-container]"
-  //   );
-  //   fetch(this.apiURL)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       const dataResult = data.response.results;
-  //       this.apiArr.push(dataResult);
-  //       this.apiArr.forEach((article) => {
-  //         const card = articleCardTemplate.content.cloneNode(true);
-  //         const header = card.querySelector("[data-header]");
-  //         console.log(article);
-  //         header.textContent = article.webTitle;
-  //         articleCardContainer.append(card);
-  //       });
-  //     });
-  // }
 }
 
 module.exports = GuardianApi;
